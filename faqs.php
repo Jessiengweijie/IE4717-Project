9b73fd715175ee01fbc3138c62cf6f837f603b2d
Alessandro <alessxdx@gmail.com>
2023-10-11 23:29:12 +0800

    content for faq

--- faqs.php
+++ faqs.php
@@ -46,11 +46,31 @@ include "assets/php/check_login.php";
             <div class="faq-container">
                 <p class="faq-header">Frequently Asked Questions</p>
                 <div class="faq-body">
-                    <a class="faq-subheader" id="faq-general" onclick="toggleContent('faq-general-content')">General</a>
-                    <div class="faq-general-content hidden" id="faq-general-content">
+                    <a class="faq-subheader" id="faq-getting_started" onclick="toggleContent('faq-getting_started-content')">Getting Started</a>
+                    <div class="faq-getting_started-content hidden" id="faq-getting_started-content">
                         <ul>
-                            <li>content 1</li>
-                            <li>content 2</li>
+                            <!-- CAN HELP ME DO THE CURSOR AND FONT SIZE FOR ALL LIKE THIS GETTING STARTED PORTION -->
+                        <li style="cursor: pointer; font-size: 22px;" onclick="toggleContent('answer1')"> 
+                            How do I sign up?
+                            <div class="hidden" id="answer1">Simply click on the 'Sign Up' option in the navigation bar at the top. Follow the prompts to provide necessary details like your name, IC, email, and more.</div><br>
+                        </li>
+                            <li  style="cursor: pointer; font-size: 22px;" onclick="toggleContent('answer2')"> What are the requirements to register?
+                                <div class="hidden" id="answer2">You'll need a valid Class 3 or 3A driving license.</div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="faq-body">
+                    <a class="faq-subheader" id="faq-account" onclick="toggleContent('faq-account-content')">Account & Membership</a>
+                    <div class="faq-account-content hidden" id="faq-account-content">
+                        <ul>
+                            <li  onclick="toggleContent('answer3')"> How do I update my profile or payment information?
+                                <div class="hidden" id="answer3">After logging in, click on the 'My Account' button in the navigation bar. From there, you can edit your personal and payment information as needed.</div><br>
+                            </li>
+                            <li  onclick="toggleContent('answer4')"> Are there membership tiers or loyalty programs?
+                                <div class="hidden" id="answer4">Currently, we don't offer membership tiers or loyalty programs. Stay tuned for future updates!</div>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -58,30 +78,62 @@ include "assets/php/check_login.php";
                 <div class="faq-body">
                     <a class="faq-subheader" id="faq-booking" onclick="toggleContent('faq-booking-content')">Booking</a>
                     <div class="faq-booking-content hidden" id="faq-booking-content">
-                        <ul>
-                            <li>content 1</li>
-                            <li>content 2</li>
+                        <ul>        
+                            <li  onclick="toggleContent('answer5')"> How do I book a car?
+                                <div class="hidden" id="answer5">Simply click the 'Book Now' button on our homepage. This will guide you through selecting your location and preferred car. After making your selections, you can complete your booking entirely online.</div><br>
+                            </li>
+                            <li  onclick="toggleContent('answer6')"> Can I modify or cancel my booking?
+                                <div class="hidden" id="answer6">HYes, you can modify or cancel your booking up to 1 hour before your scheduled start time. Cancellations made later than this will incur a $20 penalty.</div>
+                            </li>
                         </ul>
                     </div>
                 </div>
 
                 <div class="faq-body">
-                    <a class="faq-subheader" id="faq-payment" onclick="toggleContent('faq-payment-content')">Payment &
-                        Pricing</a>
+                    <a class="faq-subheader" id="faq-payment" onclick="toggleContent('faq-payment-content')">Pricing & Payment </a>
                     <div class="faq-payment-content hidden" id="faq-payment-content">
                         <ul>
-                            <li>content 1</li>
-                            <li>content 2</li>
+
+                            <li  onclick="toggleContent('answer7')"> How does the pricing work?
+                                <div class="hidden" id="answer7">Our car rental operates on an hourly basis, with rates ranging from $10 to $18 per hour, depending on the vehicle type. For example, a Honda Civic is priced at $10/hour, while a BMW 318i comes in at $15/hour. Importantly, our prices are all-inclusive; you won't face any additional charges for petrol or electricity.</div><br>
+                            </li>
+                            <li  onclick="toggleContent('answer8')"> How do I retrieve my payment receipts?
+                                <div class="hidden" id="answer8">A detailed transaction receipt, along with your booking information, will be emailed to you upon payment.</div><br>
+                            </li>
+                            <li  onclick="toggleContent('answer9')"> What payment methods are accepted?
+                                <div class="hidden" id="answer9">We accept both Visa and Mastercard.</div>
+                            </li>
                         </ul>
                     </div>
                 </div>
 
                 <div class="faq-body">
-                    <a class="faq-subheader" id="faq-collection" onclick="toggleContent('faq-collection-content')">Collection & Return</a>
-                    <div class="faq-collection-content hidden" id="faq-collection-content">
+                    <a class="faq-subheader" id="faq-locations" onclick="toggleContent('faq-locations-content')">Locations & Availability</a>
+                    <div class="faq-locations-content hidden" id="faq-locations-content">
                         <ul>
-                            <li>content 1</li>
-                            <li>content 2</li>
+                            <li  onclick="toggleContent('answer10')"> Where can I pick up or drop off the cars?
+                                <div class="hidden" id="answer10">Cars must be picked up and returned to the specific location you chose during booking. We have designated parking lots to ensure easy access for you and the next user.</div><br>
+                            </li>
+                            <li  onclick="toggleContent('answer11')"> Are cars available 24/7?
+                                <div class="hidden" id="answer11">Absolutely! Our platform and car fleet are accessible 24/7 for your convenience.</div><br>
+                            </li>
+                            <li  onclick="toggleContent('answer12')"> What if I am unable to find a car near me?
+                                <div class="hidden" id="answer12">You can submit a location request form on our site. We evaluate these requests for potential new locations. In the meantime, only the locations listed on our site are available.</div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="faq-body">
+                    <a class="faq-subheader" id="vehicle" onclick="toggleContent('faq-vehicle-content')">Vehicle Usage & Maintenance</a>
+                    <div class="faq-vehicle-content hidden" id="faq-vehicle-content">
+                        <ul>
+                            <li  onclick="toggleContent('answer12')"> What if the vehicle has damage or is dirty when I start my trip?
+                                <div class="hidden" id="answer12">Before starting your journey, inspect the car. If you find damages or cleanliness issues, snap a photo and email it to us. We'll guide you on the next steps, ensuring you have a pleasant experience. Refunds or adjustments will be made if necessary.</div><br>
+                            </li>
+                            <li  onclick="toggleContent('answer12')"> How does fueling or charging work?
+                                <div class="hidden" id="answer12">Fuel and charging expenses are on us! Every car is equipped with a cash card for fuel and charging station payment</div>
+                            </li>
                         </ul>
                     </div>
                 </div>
